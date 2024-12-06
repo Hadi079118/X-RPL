66d3c79b7c50f7abdbde9cf1df9826f3b5b46326
Hadi079118 <mashadi9118@gmail.com>
2024-12-06 14:05:33 +0700

    Delete tugasarray1.java

--- tugasarray1.java
+++
@@ -1,7 +0,0 @@
-public class tugasarray1 {
-    public static void main(String[] args) {
-        String[] namaSiswa = {"Muwafiq", "Riyan", "Pratama" };
-
-        System.out.println(namaSiswa);
-    }
-}
