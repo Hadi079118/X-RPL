419ede267bca2d4971823035e3791c289be8e84e
Mas Hadi Wijaya <mashadi9118@gmail.com>
2025-06-02 22:07:30 +0700

    Add files via upload

---
+++ js/coba.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class coba {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("pilih nama berikut: a.adi b.ad c.a");
+        System.out.println("Tentukan nama: ");
+        String nama = input.next();
+
+        
+    }
+}
---
+++ js/portfolio/main.js
@@ -0,0 +1,9 @@
+/* alert("hayho")
+let usia = prompt("berapa umur anda?")
+let nama = prompt('siapa nama anda?')
+alert('usia anda adalah ' + usia + ' dan nama anda adalah ' + nama)
+console.log('halo usia kamu adalah ' + usia ) 
+for(let i = 1; i <= 10; i = i + 1) {
+    console.log('nama anda adalah ' + nama)
+
+} *\        
